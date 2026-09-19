c5a2617c0b7d4fd99750f2fa0561f8144163339a
mmjt2000 <jeant.montreuil@gmail.com>
2026-09-19 14:09:06 -0400

    Fix import

--- app.py
+++ app.py
@@ -8,8 +8,19 @@ import os
 import streamlit.components.v1 as components
 
 from pronunciation_data import PRONUNCIATION_DATA
-from pronunciation_data import PRONUNCIATION_DATA
-from sheets_db import (get_all_sessions, get_session_by_num, update_session, get_stats, authenticate, save_submission, get_student_submissions, get_pending_submissions, get_all_submissions_with_feedback, save_feedback, change_password, get_lang, save_audio_submission)
+from sheets_db import get_all_sessions
+from sheets_db import get_session_by_num
+from sheets_db import update_session
+from sheets_db import get_stats
+from sheets_db import authenticate
+from sheets_db import save_submission
+from sheets_db import get_student_submissions
+from sheets_db import get_pending_submissions
+from sheets_db import get_all_submissions_with_feedback
+from sheets_db import save_feedback
+from sheets_db import change_password
+from sheets_db import get_lang
+from sheets_db import save_audio_submission
 
 st.set_page_config(
     page_title="Lingua Bridge Academy",
